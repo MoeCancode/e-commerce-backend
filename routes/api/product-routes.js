7f78dc48109a2e4d13f913e7dd93f5837869e6b9
M A Razvi <abuzarflw@gmail.com>
2022-05-06 00:18:50 -0700

    Add functionality to Product get routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    let productData = await Product.findAll({
+      include : [
+        {model: Category}, {model: Tag}
+      ]
+    })
+    res.status(200).json(productData);
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    let productData = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include : [{model: Category}, {model: Tag}]
+    })
+    
+    if(!productData) {
+      res.status(404).status({message: "No product found with that name!"});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
