e3d7b2367655e6e357e9c2e495c604f2d3c1b54f
M A Razvi <abuzarflw@gmail.com>
2022-05-07 00:33:00 -0700

    Add async to functions using await. Fix Product to Tag relationship

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     let categoryData = await Category.create({
@@ -52,7 +52,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     let categoryData = await Category.update(
@@ -76,7 +76,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     let categoryData = await Category.destroy({
