3ccc393f3950869b6b55ca88a3293069d0ff31e3
M A Razvi <abuzarflw@gmail.com>
2022-05-06 22:51:40 -0700

    Fix typo in Category model

--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,7 @@ Product.init(
       allowNull: false
     },
    price: {
-    type: DataTypes.DECIMAL,
+    type: DataTypes.DECIMAL(10,2),
     allowNull: false,
     validate: {
       isDecimal: true,
@@ -37,7 +37,7 @@ Product.init(
    category_id: {
     type: DataTypes.INTEGER,
     references: {
-      model: "Category",
+      model: "category",
       key: "id"
     }
    }
